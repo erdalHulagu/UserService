4a85036d26ad599d638634e18b8d1f6c538fa1fe
erdalHulagu <erdalhulahu@gmail.com>
2025-07-31 01:31:12 +0100

    31/07/25

--- src/main/java/com/erdal/UserBookingServiceApplication.java
+++ src/main/java/com/erdal/UserBookingServiceApplication.java
@@ -2,8 +2,10 @@ package com.erdal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserBookingServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/erdal/controller/UserController.java
+++ src/main/java/com/erdal/controller/UserController.java
@@ -4,7 +4,6 @@ package com.erdal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
